f61a5c15bf4f81327117ae20a71378e9391bfd75
Jonas Bird <jonas.bird.m@gmail.com>
2022-07-30 21:13:36 -0400

    FEAT modify index.js to log the url requested
    remove the static example routes

--- index.js
+++ index.js
@@ -3,37 +3,46 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  if (req.url === '/') {
-    fs.readFile(
-      path.join(__dirname, 'public', 'index.html'),
-      (err, content) => {
-        if(err) throw err;
-        res.writeHead(200, { 'Content-Type': 'text/html'});
-        res.end(content);
-    }
-    );
-  }
+  // if (req.url === '/') {
+  //   fs.readFile(
+  //     path.join(__dirname, 'public', 'index.html'),
+  //     (err, content) => {
+  //       if(err) throw err;
+  //       res.writeHead(200, { 'Content-Type': 'text/html'});
+  //       res.end(content);
+  //   }
+  //   );
+  // }
 
-  if (req.url === '/about') {
-    fs.readFile(
-      path.join(__dirname, 'public', 'about.html'),
-      (err, content) => {
-        if(err) throw err;
-        res.writeHead(200, { 'Content-Type': 'text/html'});
-        res.end(content);
-    }
-    );
-  }
+  // if (req.url === '/about') {
+  //   fs.readFile(
+  //     path.join(__dirname, 'public', 'about.html'),
+  //     (err, content) => {
+  //       if(err) throw err;
+  //       res.writeHead(200, { 'Content-Type': 'text/html'});
+  //       res.end(content);
+  //   }
+  //   );
+  // }
 
-  if (req.url === '/api/users') {
-    const users = [
-      { name: 'Bob Smith', age: 40 },
-      { name: 'John Doe', age: 30 }
-    ];
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    res.end(JSON.stringify(users));
-  }
+  // if (req.url === '/api/users') {
+  //   const users = [
+  //     { name: 'Bob Smith', age: 40 },
+  //     { name: 'John Doe', age: 30 }
+  //   ];
+  //   res.writeHead(200, { 'Content-Type': 'application/json' });
+  //   res.end(JSON.stringify(users));
+  // }
 
+  // Build file paths
+  let filePath = path.join(
+    __dirname,
+    'public',
+    req.url === '/' ? 'index.html' : req.url
+  );
+
+  console.log(filePath);
+  res.end();
   });
 
 // default to listening on port 5000, unless the port is specified by the
