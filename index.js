df62dc079b21d45cc3150bb651055211fd0f5fdb
Jonas Bird <jonas.bird.m@gmail.com>
2022-07-30 21:07:07 -0400

    FEAT Add html and json content
    modify index,js to use an if statement to serve up example html pages
    and some JSON data

--- index.js
+++ index.js
@@ -3,9 +3,37 @@ const path = require('path');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  if(req.url === '/') {
-    res.end('<h1>Home</h1>');
+  if (req.url === '/') {
+    fs.readFile(
+      path.join(__dirname, 'public', 'index.html'),
+      (err, content) => {
+        if(err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html'});
+        res.end(content);
+    }
+    );
   }
+
+  if (req.url === '/about') {
+    fs.readFile(
+      path.join(__dirname, 'public', 'about.html'),
+      (err, content) => {
+        if(err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html'});
+        res.end(content);
+    }
+    );
+  }
+
+  if (req.url === '/api/users') {
+    const users = [
+      { name: 'Bob Smith', age: 40 },
+      { name: 'John Doe', age: 30 }
+    ];
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(users));
+  }
+
   });
 
 // default to listening on port 5000, unless the port is specified by the
